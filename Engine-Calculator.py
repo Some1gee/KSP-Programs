416f54943a70225b36c6b2df6eb026ad63e18d56
Some1gee <49211586+Some1gee@users.noreply.github.com>
2019-11-15 13:06:37 -0800

    Update Engine-Calculator.py
    Removed unnecessary while loop (Fixed TWR). Removed the 1.8 TWR Maximum limit for SPEEEEEED. Added rounding to dV and TWR for simplicity of the final list. Added the Making History Sea Level Engines.
    Current Issues: TWR and dV are both a tiny bit off. A fix is unnecessary as they are off about ~0.001 for TWR and 1-2 dV.
    To do:
    Sea Level:
    Add Variable Gravity for different Planets on Sea Level.
    Add Part Sizes
    Add Thrust Vectoring

--- Engine-Calculator.py
+++ Engine-Calculator.py
@@ -15,7 +15,7 @@ M11 = M1*1000 #Convert to Kg
 #Name, Isp, Thrust in Kn
 def sL(M0, M1, dVreq, M01, M11):
   #List of Sea Level Engine Stats
-  sLEngines = [("Spider", 260, 1.79, 0.02), ("Twitch", 275, 15, 0.08),("Thud", 275, 108.20, 0.9), ("Puff", 120, 9.60, 0.09), ("Ant", 80, 0.51, 0.02), ("Spark", 265, 16.56, 0.13), ("Terrier",85, 14.78, 0.50), ("Reliant",265,205.16, 1.25), ("Swivel", 250,167.97, 1.5), ("Vector", 295, 936.51, 4), ("Dart", 290, 153.53, 1), ("Nerv", 185, 13.88, 3), ("Poodle", 90, 64.29, 1.75), ("Skipper", 280, 568.75, 3), ("Mainsail", 285, 1379.03, 6), ("Twin Boar", 280, 1866.67, 10.5), ("Rhino", 205, 1205.88, 9), ("Mammoth",295, 3746.03, 15), ("Rapier", 275, 162.295, 2)]
+  sLEngines = [("Spider", 260, 1.79, 0.02), ("Twitch", 275, 15, 0.08),("Thud", 275, 108.20, 0.9), ("Puff", 120, 9.60, 0.09), ("Ant", 80, 0.51, 0.02), ("Spark", 265, 16.56, 0.13), ("Terrier",85, 14.78, 0.50), ("Reliant",265,205.16, 1.25), ("Swivel", 250,167.97, 1.5), ("Vector", 295, 936.51, 4), ("Dart", 290, 153.53, 1), ("Nerv", 185, 13.88, 3), ("Poodle", 90, 64.29, 1.75), ("Skipper", 280, 568.75, 3), ("Mainsail", 285, 1379.03, 6), ("Twin Boar", 280, 1866.67, 10.5), ("Rhino", 205, 1205.88, 9), ("Mammoth",295, 3746.03, 15), ("Rapier", 275, 162.295, 2), ("Mastadon", 290, 1283.63, 5), ("Cheetah", 150, 52.82, 1), ("Bobcat", 290, 374.19, 2), ("Skiff", 265, 240.91, 1.6), ("Wolfhound", 70, 69.08, 3.3), ("Kodiak", 285, 247, 1.25), ("Cub", 280, 28.9, 0.18)]
   sLDv = []
   TWRsL = []
   engineCount = 1
@@ -27,12 +27,9 @@ def sL(M0, M1, dVreq, M01, M11):
     WeightT = i[3]
     WeightkG = i[3] * 1000
     twr = ThrustkN1/((M0+(WeightT))*9.81)
-    if twr < 1.2:
-        while twr < 1.2: #Should figure out how many engines needed to get a 1.2 TWR
-            engineCount = int(1.2/twr) #ints will round down until 0.99999999999 (11 9s)
-            twr = twr*engineCount #applying the change to TWR
-            break
-    dV = ((ISP1*9.807)*math.log(((M01+(engineCount*WeightkG)))/(M11+(engineCount*WeightkG)), math.e))
+    engineCount = int(1.2/twr)+1 #ints will round down until 0.99999999999 (11 9s)
+    twr = round(twr*engineCount, 2) #applying the change to TWR
+    dV = round(((ISP1*9.807)*math.log(((M01+(engineCount*WeightkG)))/(M11+(engineCount*WeightkG)), math.e)))+1
     #dV = ISP*Gravitational Constant * ln(Wet Mass+Engine Weight/Dry Mass + Engine Weight)
     sLDv.append((engineCount, Name1, "DeltaV: ", dV, "TWR: ", twr))
   print("Possible Engines: ")
@@ -40,8 +37,8 @@ def sL(M0, M1, dVreq, M01, M11):
   for i in sLDv: #Searches  from the new list
     engAmount = i[0] #Engine Amount
     engName = i[1] #Name
-    dV = i[3] #dV
-    twrF = i[5] #TWR
+    dV = int(i[3]) #dV
+    twrF = round(i[5], 1) #TWR
     if engAmount <= 6 and dV >= dVreq and 1.2 <= twrF:
       GoodSL.append(("Engine Name: ", engName, "Engine Count: ", engAmount, "DeltaV: ", dV, "TWR: ", twrF)) #New List of Actual possible engines
   print(GoodSL)
