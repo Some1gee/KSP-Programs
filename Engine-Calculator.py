3363cebdcae6586bc5fc0f20b01995b4545b920b
Some1gee <49211586+Some1gee@users.noreply.github.com>
2019-11-16 15:28:24 -0800

    Update Engine-Calculator.py
    Adding Variable Sizes

--- Engine-Calculator.py
+++ Engine-Calculator.py
@@ -15,14 +15,15 @@ vac = 0
 maxEngineCount = int(input("Maximum acceptable number of engines: "))
 reqVecRange = float(input("What is the Minimum Required Vector Range (0 and above): "))
 radialAsk = str(input("Are radial engines acceptable? Answer with 'True', 'False', or 'Only': "))
+sizeAsk = float(input("Maximum Possible Engine Size (m):"))
 M01 = M0*1000 #Convert to Kg
 M11 = M1*1000 #Convert to Kg
 #Name, Isp, Thrust in Kn
-def engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVecRange, radialAsk):
+def engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVecRange, radialAsk, sizeAsk):
   #List of Sea Level Engine Stats
   if vac == False:
-    sLEngines = [("Spider", 260, 1.79, 0.02, 10, 0), ("Twitch", 275, 15, 0.08, 8, 0),("Thud", 275, 108.20, 0.9, 8, 0), ("Puff", 120, 9.60, 0.09, 6, 0), ("Ant", 80, 0.51, 0.02, 0, 1), ("Spark", 265, 16.56, 0.13, 3, 1), ("Terrier",85, 14.78, 0.50, 4, 1), ("Reliant",265,205.16, 1.25, 0, 1), ("Swivel", 250,167.97, 1.5, 3, 1), ("Vector", 295, 936.51, 4, 10.5, 1), ("Dart", 290, 153.53, 1, 0, 1), ("Nerv", 185, 13.88, 3, 0, 1), ("Poodle", 90, 64.29, 1.75, 4.5, 1), ("Skipper", 280, 568.75, 3, 2, 1), ("Mainsail", 285, 1379.03, 6, 2, 1), ("Twin Boar", 280, 1866.67, 10.5, 1.5, 1), ("Rhino", 205, 1205.88, 9, 4, 1), ("Mammoth",295, 3746.03, 15, 2, 1), ("Rapier", 275, 162.295, 2, 3, 1), ("Mastadon", 290, 1283.63, 5, 5, 1), ("Cheetah", 150, 52.82, 1, 4, 1), ("Bobcat", 290, 374.19, 2, 5, 1), ("Skiff", 265, 240.91, 1.6, 2, 1), ("Wolfhound", 70, 69.08, 3.3, 3, 1), ("Kodiak", 285, 247, 1.25, 0, 1), ("Cub", 280, 28.9, 0.18, 22.5, 0)]
-    #Name, ISP, Thrust in kN, Weight in Tons, Vectoring Range, Radial (0 = True)
+    sLEngines = [("Spider", 260, 1.79, 0.02, 10, 0, 0, 0), ("Twitch", 275, 15, 0.08, 8, 0, 0, 0),("Thud", 275, 108.20, 0.9, 8, 0, 0), ("Puff", 120, 9.60, 0.09, 6, 0, 0), ("Ant", 80, 0.51, 0.02, 0, 1, 0), ("Spark", 265, 16.56, 0.13, 3, 1, 0.6), ("Terrier",85, 14.78, 0.50, 4, 1, 1.25), ("Reliant",265,205.16, 1.25, 0, 1, 1.25), ("Swivel", 250,167.97, 1.5, 3, 1, 1.25), ("Vector", 295, 936.51, 4, 10.5, 1, 1.25), ("Dart", 290, 153.53, 1, 0, 1.25), ("Nerv", 185, 13.88, 3, 0, 1, 1.25), ("Poodle", 90, 64.29, 1.75, 4.5, 1, 2.5), ("Skipper", 280, 568.75, 3, 2, 1, 2.5), ("Mainsail", 285, 1379.03, 6, 2, 1, 2.5), ("Twin Boar", 280, 1866.67, 10.5, 1.5, 1, 2.5), ("Rhino", 205, 1205.88, 9, 4, 1, 3.75), ("Mammoth",295, 3746.03, 15, 2, 1, 3.75), ("Rapier", 275, 162.295, 2, 3, 1, 1.25), ("Mastadon", 290, 1283.63, 5, 5, 1, 2.5), ("Cheetah", 150, 52.82, 1, 4, 1, 1.25), ("Bobcat", 290, 374.19, 2, 5, 1, 1.875), ("Skiff", 265, 240.91, 1.6, 2, 1, 2.5), ("Wolfhound", 70, 69.08, 3.3, 3, 1, 1.875), ("Kodiak", 285, 247, 1.25, 0, 1, 1.25), ("Cub", 280, 28.9, 0.18, 22.5, 0, 0)]
+    #Name, ISP, Thrust in kN, Weight in Tons, Vectoring Range, Radial Mounted (0 = True), Size
     sLDv = []
     TWRsL = []
     engineCount = 1
@@ -35,12 +36,13 @@ def engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVec
       WeightkG = i[3] * 1000
       vectorRange1 = i[4]
       radialTF = i[5]
+      size1 = i[6]
       twr = ThrustkN1/((M0+(WeightT))*constOfGrav)#Original TWR Calc
       engineCount = int((1.35/twr)+1) #ints will round down until 0.99999999999 (11 9s)
       twr = round((ThrustkN1*engineCount)/((M0+(WeightT*engineCount))*constOfGrav), 2)#Second TWR Calc
       dV = round(((ISP1*constOfGrav)*math.log(((M01+(engineCount*WeightkG)))/(M11+(engineCount*WeightkG)), math.e)))
       #dV = ISP*Gravitational Constant * ln(Wet Mass+Engine Weight/Dry Mass + Engine Weight)
-      sLDv.append((engineCount, Name1, "DeltaV: ", dV, "TWR: ", twr, "Vector Range: ", vectorRange1, "Radial True/False:", radialTF))
+      sLDv.append((engineCount, Name1, "DeltaV: ", dV, "TWR: ", twr, "Vector Range: ", vectorRange1, "Radial True/False:", radialTF, "Size (m):", size1))
       #Add it to Sea level Delta V List (Name was created before it's final purpose was)
     print("Possible Engines: ")
     GoodSL = []
@@ -51,6 +53,7 @@ def engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVec
       twrF = round(i[5], 1) #TWR
       vectorRange2 = i[7]
       radialTF2 = i[9]
+      size2 = i[11]
       if radialTF2 == 0 and radialAsk == "Only": #Changing radialTF2 from the opposite of Binary to Boolean
         radialTF2 = "Only"
       elif radialTF2 == 1 and radialAsk == "Only": #Engine is not Radial and Radial Ask is Only Radial
@@ -63,7 +66,7 @@ def engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVec
         radialTF2 = "No"
       else: #Engine is not radial and Radial Ask is not radial
         radialTF2 = "False"
-      if engAmount <= maxEngineCount and dV >= dVreq and 1.2 <= twrF and vectorRange2 >= reqVecRange and radialTF2 == radialAsk: 
+      if engAmount <= maxEngineCount and dV >= dVreq and 1.2 <= twrF and vectorRange2 >= reqVecRange and radialTF2 == radialAsk and size2 <= sizeAsk: 
         GoodSL.append(("Engine Name: ", engName, "Engine Count: ", engAmount, "DeltaV: ", dV, "TWR: ", twrF, "Vector Range: ", vectorRange2)) #New List of Actual possible engines
     print(GoodSL)
     print(" ")#Space
@@ -128,5 +131,5 @@ elif constOfGrav == "Jool" or "Laythe":
 else:#This currently doesn't work
   print("Try Again")
   constOfGrav = str(input("Name of Celestial Body in which you are in: (Kerbol, Moho, Eve, Gilly, Kerbin, Mun, Minmus, Duna, Ike, Dres, Jool, Laythe, Vall, Tylo, Bop, Pol, Eeloo) "))
-engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVecRange, radialAsk) #calling function
+engineCalc(M0, M1, dVreq, M01, M11, constOfGrav, vac, maxEngineCount, reqVecRange, radialAsk, sizeAsk) #calling function
 #Kgt = kN*101.9716005
