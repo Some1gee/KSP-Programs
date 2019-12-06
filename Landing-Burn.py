444b9e7bc1e8c6b0e72ba6e27471055282b8046a
Some1gee <49211586+Some1gee@users.noreply.github.com>
2019-12-05 18:15:58 -0800

    Update Landing-Burn.py
    i2 Fixed this

--- Landing-Burn.py
+++ Landing-Burn.py
@@ -6,6 +6,8 @@ conn = krpc.connect(name='Landing Burn')
 vessel = conn.space_center.active_vessel
 space_center = conn.space_center
 
+srf_frame = vessel.orbit.body.reference_frame
+
 altitude = conn.add_stream(getattr, vessel.flight(), "mean_altitude")
 
 gravConst = float(vessel.orbit.body.surface_gravity)
@@ -17,31 +19,31 @@ ExhaustVelocity = vessel.specific_impulse*gravConst
 
 DeltaVelocity = (ExhaustVelocity)*math.log(vessel.mass/vessel.dry_mass)
 
-DeltaTime = ((vessel.mass*ExhaustVelocity)/vessel.available_thrust)*(1-math.e**(-DeltaVelocity/ExhaustVelocity))
-
-acceleration = (vessel.mass/vessel.available_thrust)*DeltaTime
-
-finalVelocity = (int(vessel.speed)+int(acceleration))
+finalVelocity = 0
 
 finalAltitude = 0
 
-accelerationDueToThrust = (vessel.mass*gravConst)-vessel.available_thrust
+speed = vessel.flight(srf_frame).speed
 
-equation = ((accelerationDueToThrust/gravConst)**2)-(2((vessel.speed**2)-(finalVelocity**2))/(4*(gravConst)*(altitude-finalAltitude)))*(twr/gravConst)-(1+(((2*(vessel.speed))*2)-((2*(finalVelocity))*2))/((4*gravConst)*(altitude-finalAltitude)))
+accelerationDueToThrust = (vessel.mass*gravConst)-vessel.available_thrust
 
 thrustNeeded = (accelerationDueToThrust/gravConst)-vessel.mass                  
 
-#ax^2 + bx + c = 0
+a = 1**2
 
+#ax^2 + bx + c = 0
+print(vessel.flight(srf_frame).speed)
 a = 1
-b = -1*(2((vessel.speed**2)-(finalVelocity**2))/(4*(gravConst)*(altitude-finalAltitude)))
-c = -(1+((2*((vessel.speed**2)-(finalVelocity**2))))/((4*gravConst)*(altitude-finalAltitude)))
+b = -1*(2((vessel.flight(srf_frame).speed**2)-(finalVelocity**2))/(4*(gravConst)*(altitude-finalAltitude)))
+c = -1*(1+((2*((vessel.flight(srf_frame).speed**2)-(finalVelocity**2))))/((4*gravConst)*(altitude-finalAltitude)))
 x = accelerationDueToThrust/gravConst
 #(-b +- sqrt(b^2 - 4ac))/2a
 root1 = ((-1*b) + math.sqrt((b**2)-(4*a*c)))/(2*a)
 root2 = ((-1*b) - math.sqrt((b**2)-(4*a*c)))/(2*a)
 
-drag = vessel.flight.drag()
+drag = vessel.flight(srf_frame).drag()
+DRAG = math.sqrt(drag[0]**2+drag[1]**2+drag[2]**2)
+
 
 if root1 > 0:
     accelerationDueToThrust = root1
